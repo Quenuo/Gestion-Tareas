58b896fd09bd9c6953eaab36671bb898f5a89542
Quenuo <angelsanchezvallejomoreno@gmail.com>
2025-05-20 21:12:07 +0200

    feat:Continuación del gestor de tareas
    --Creacion del compose.yml,
    --Creado el modelo Task
    --Creado el archivo .env para la contraseña y usuarios de postgresSQL

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,7 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//clase main
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
